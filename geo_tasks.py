43f6739ea2e343a5d6cea412eb8159387256c139
Andrpocc <Andrey.pochatkov@mail.ru>
2020-10-17 14:27:03 +0300

    Обновление модуля решения простых задач

--- geo_tasks.py
+++ geo_tasks.py
@@ -1,25 +1,47 @@
 import math
-from sympy import Segment, Point
+from random import normalvariate
+
 import numpy as np
-import pprint
+import pandas as pd
+from sympy import Segment, Point
 
 
-def to_degrees(degrees: int, minutes: float, seconds: float = 0):
+def to_degrees(degrees: int, minutes: float, seconds: float = 0) -> float:
+    """
+    Преобразование dms -> d
+    :param degrees: Градусы
+    :param minutes: Минуты
+    :param seconds: Секунды
+    :return: Градусы
+    """
     return degrees + minutes / 60 + seconds / 3600
 
 
-def to_d_m_s(degrees: float):
+def to_d_m_s(degrees: float) -> tuple:
+    """
+    Преобразвоание d -> dms
+    :param degrees: Градусы
+    :return: Кортеж (градусы, минуты, секунды)
+    """
     d = math.trunc(degrees)
     m = math.trunc((degrees - d) * 60)
     s = round(((degrees - d) - m / 60) * 60 * 60)
-    return [d, m, s]
+    return d, m, s
 
 
-def ogz_points(xa, ya, xb, yb):
-    """Обратная геодезическая задача"""
+def ogz_points(xa: float, ya: float, xb: float, yb: float) -> tuple:
+    """
+    Обратная геодезическая задача для координат пунктов
+    :param xa: Координата х первого пункта линии
+    :param ya: Координата у первого пункта линии
+    :param xb: Координата х второго пункта линии
+    :param yb: Координата у второго пункта линии
+    :return: Кортеж (градусы, минуты, секунды, горизонтальное проложение)
+    """
     delx = xb - xa
     dely = yb - ya
-    S = math.sqrt(delx ** 2 + dely ** 2)
+    s = math.sqrt(delx ** 2 + dely ** 2)
+
     if dely == 0 and delx > 0:
         alf = 0
     elif dely == 0 and delx < 0:
@@ -33,27 +55,29 @@ def ogz_points(xa, ya, xb, yb):
 
         if delx > 0 and dely > 0:
             alf = rumb
-
         elif delx < 0 and dely > 0:
             alf = 180 - rumb
-
         elif delx < 0 and dely < 0:
             alf = 180 + rumb
-
         elif delx > 0 and dely < 0:
             alf = 360 - rumb
 
-    G = math.trunc(alf)
-    M = math.trunc((alf - G) * 60)
-    C = round(((alf - G) - M/60) * 60 * 60)
-    return G, M, C, S
+    g = math.trunc(alf)
+    m = math.trunc((alf - g) * 60)
+    c = round(((alf - g) - m / 60) * 60 * 60)
+    return g, m, c, s
 
 
-def ogz_delta(dx, dy):
-    """Обратная геодезическая задача"""
+def ogz_delta(dx: float, dy: float) -> tuple:
+    """
+    Обратная геодезическая задача для приращений координат
+    :param dx: Приращение по х
+    :param dy: Приращенеи по у
+    :return: Кортеж (дирекционный угол, горизонтальное приложение)
+    """
     delx = dx
     dely = dy
-    S = math.sqrt(delx ** 2 + dely ** 2)
+    s = math.sqrt(delx ** 2 + dely ** 2)
     if dely == 0 and delx > 0:
         alf = 0
     elif dely == 0 and delx < 0:
@@ -64,61 +88,82 @@ def ogz_delta(dx, dy):
         alf = 270
     else:
         rumb = math.fabs(math.degrees(math.atan(dely / delx)))
-
         if delx > 0 and dely > 0:
             alf = rumb
-
         elif delx < 0 and dely > 0:
             alf = 180 - rumb
-
         elif delx < 0 and dely < 0:
             alf = 180 + rumb
-
         elif delx > 0 and dely < 0:
             alf = 360 - rumb
-    return alf
+    return alf, s
 
 
-def pgz(X1, Y1, G, M, C, S):
-    """Прямая геодезическая задача"""
-    angle = G + M/60 + C/(60*60)
+def pgz(x1: float, y1: float, g: int, m: int, c: float, s: float) -> tuple:
+    """
+    Прямая геодезическая задача
+    :param x1: Координата х пункта
+    :param y1: Координата у пункта
+    :param g: Дирекционный угол (градусы)
+    :param m: Дирекционный угол (минуты)
+    :param c: Дирекционный угол (секунды)
+    :param s: Горизонтальное проложение
+    :return: Кортеж координат вычисляемого пункта
+    """
+    angle = g + m / 60 + c / (60 * 60)
     angle = math.radians(angle)
-    X2 = X1 + S * math.cos(angle)
-    Y2 = Y1 + S * math.sin(angle)
-    return X2, Y2
+    x2 = x1 + s * math.cos(angle)
+    y2 = y1 + s * math.sin(angle)
+    return x2, y2
 
 
-def polygon_square(points):
-    """Площадь полигона по координатам точек"""
+def polygon_square(points: list) -> float:
+    """
+    Площадь полигона по координатам его вершин
+    Формула Гаусса
+    :param points: Список координат вершин полигона
+    :return: Значение площади
+    """
     sx = 0
     sy = 0
     n = len(points)
-
     for i in range(n):
-        if i != n-1:
-            sx += points[i][0] * points[i+1][1]
-        elif i == n-1:
+        if i != n - 1:
+            sx += points[i][0] * points[i + 1][1]
+        elif i == n - 1:
             sx += points[i][0] * points[0][1]
-
     for i in range(n):
-        if i != n-1:
-            sy -= points[i][1] * points[i+1][0]
-        elif i == n-1:
+        if i != n - 1:
+            sy -= points[i][1] * points[i + 1][0]
+        elif i == n - 1:
             sy -= points[i][1] * points[0][0]
+    square = math.fabs(sx + sy) / 2
+    return square
 
-    s = math.fabs(sx + sy) / 2
-    return s
 
-
-def midpoint(x1, y1, x2, y2):
-    """Координаты середины отрезка"""
-    xm = (x1 + x2)/2
-    ym = (y1 + y2)/2
+def midpoint(x1: float, y1: float, x2: float, y2: float) -> tuple:
+    """
+    Координаты середины отрезка
+    :param x1: Координата х первого пункта
+    :param y1: Координата у первого пункта
+    :param x2: Координата х второго пункта
+    :param y2: Координата у второго пункта
+    :return: Кортеж координат середины отрезка
+    """
+    xm = (x1 + x2) / 2
+    ym = (y1 + y2) / 2
     return xm, ym
 
 
-def intersection_of_segments(p1_x, p1_y, p2_x, p2_y):
-    """Координаты точки пересечения двух отрезков"""
+def intersection_of_segments(p1_x: Point, p1_y: Point, p2_x: Point, p2_y: Point) -> tuple:
+    """
+    Координаты точки пересечения двух отрезков
+    :param p1_x: Первая точка первого отрезка
+    :param p1_y: Вторая точка первого отрезка
+    :param p2_x: Первая точка второго отрезка
+    :param p2_y: Вторая точка второго отрезка
+    :return: Кортеж координат точки пересечения отрезков
+    """
     s1 = Segment(p1_y, p1_x)
     s2 = Segment(p2_y, p2_x)
     intersection = s1.intersection(s2)
@@ -129,7 +174,19 @@ def intersection_of_segments(p1_x, p1_y, p2_x, p2_y):
         return 0, 0
 
 
-def adjustment_theodolite_course(first_directional_angle: float, last_directional_angle: float, angles: list, horizontal_layings: list, first_point: list, last_point: list, left_angle=True):
+def adjustment_traverse(first_directional_angle: float, last_directional_angle: float, angles: list,
+                        horizontal_layings: list, first_point: list, last_point: list, left_angle=True) -> pd.DataFrame:
+    """
+    Раздельный метод уравнивание теодолитного хода
+    :param first_directional_angle: Дирекционный угол исходной линии хода
+    :param last_directional_angle: Дирекционный угол замыкающей линии хода
+    :param angles: Горизонтальные углы
+    :param horizontal_layings: Горизонтальные проложения
+    :param first_point: Начальный опорный пункт
+    :param last_point: Замыкающий опорный пункт
+    :param left_angle: По умолчанию горизонтальные углы приняты за левые
+    :return: DataFrame уравненных координат пунктов хода
+    """
     angles_array = np.array(angles)
     horizontal_layings_array = np.array(horizontal_layings)
     number_of_angles = len(angles)
@@ -142,12 +199,18 @@ def adjustment_theodolite_course(first_directional_angle: float, last_directiona
     correction = -angular_residual / number_of_angles
     corrected_angles = angles_array + correction
     if left_angle:
-        directional_angles = []
+        directional_angles = list()
         directional_angles.append(first_directional_angle + corrected_angles[0] - 180)
         for i in range(len(corrected_angles) - 2):
-            directional_angles.append(directional_angles[i] + corrected_angles[i + 1] - 180)
+            dir_angle = directional_angles[i] + corrected_angles[i + 1] - 180
+            if dir_angle > 360:
+                directional_angles.append(dir_angle - 360)
+            elif dir_angle < 0:
+                directional_angles.append(dir_angle + 360)
+            else:
+                directional_angles.append(dir_angle)
     else:
-        directional_angles = []
+        directional_angles = list()
         directional_angles.append(first_directional_angle - corrected_angles[0] + 180)
         for i in range(len(corrected_angles) - 2):
             directional_angles.append(directional_angles[i] - corrected_angles[i + 1] + 180)
@@ -163,23 +226,31 @@ def adjustment_theodolite_course(first_directional_angle: float, last_directiona
     sum_of_layings = sum(horizontal_layings)
     correction_delta_x = -delta_x_residual * horizontal_layings_array / sum_of_layings
     correction_delta_y = -delta_y_residual * horizontal_layings_array / sum_of_layings
-    corrected_delta_x = delta_x_array + correction_delta_x
-    corrected_delta_y = delta_y_array + correction_delta_y
-    x_coordinates = []
-    x_coordinates.append(first_point[0] + corrected_delta_x[0])
+    corrected_delta_x = np.round(delta_x_array + correction_delta_x, 3)
+    corrected_delta_y = np.round(delta_y_array + correction_delta_y, 3)
+    x_coordinates = list()
+    x_coordinates.append(round(first_point[0] + corrected_delta_x[0], 3))
     for i in range(len(corrected_delta_x) - 2):
         x_coordinates.append(x_coordinates[i] + corrected_delta_x[i + 1])
-    y_coordinates = []
-    y_coordinates.append(first_point[1] + corrected_delta_y[0])
+    y_coordinates = list()
+    y_coordinates.append(round(first_point[1] + corrected_delta_y[0], 3))
     for i in range(len(corrected_delta_y) - 2):
         y_coordinates.append(y_coordinates[i] + corrected_delta_y[i + 1])
-    return [x_coordinates, y_coordinates]
+    coordinates_of_traverse = pd.DataFrame()
+    coordinates_of_traverse['x'] = x_coordinates
+    coordinates_of_traverse['y'] = y_coordinates
+    return coordinates_of_traverse
 
-right_angles = [to_degrees(302, 40), to_degrees(159, 25.5), to_degrees(189, 52.5,),
-                to_degrees(268, 30.5)]
-layings = [235.84, 415, 236]
-first_point = [6093.62, 1984.51]
-last_point = [5562.24, 2682.9]
-answer = adjustment_theodolite_course(to_degrees(237, 26.1), to_degrees(36, 58.3), right_angles, layings, first_point, last_point, left_angle=False)
-pprint.pprint(answer)
 
+def generate_errors(mu: float, count: int) -> list:
+    """
+    Генерация списка псевдослучайных ошибок,
+    подчиняющихся нормальному закону распределения
+    :param mu: среднее квадартическое отклонение
+    :param count: Количество ошибок для генерации
+    :return: Список ошибок
+    """
+    errors = list()
+    for i in range(count):
+        errors.append(round(normalvariate(0, mu), 0))
+    return errors
