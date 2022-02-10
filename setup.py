a162e7b0bd3cde2e2c67d262ec0165fd9bf15a1a
andrpocc <andrey.pochatkov@mail.ru>
2022-02-10 22:35:04 +0300

    v0.0.5

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+requirements = ["numpy", "sympy"]
+
 setuptools.setup(
     name="pygeoguz",
-    version="0.0.4",
+    version="0.0.5",
     author="Andrey Pochatkov",
     author_email="andrey.pochatkov@mail.ru",
     description="Solution of the geodesy tasks in Python",
@@ -18,5 +20,6 @@ setuptools.setup(
     ],
     keywords=['geodesy'],
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     python_requires='>=3.7',
 )
